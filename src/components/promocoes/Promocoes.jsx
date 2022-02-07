5fd1b49b40383aeb396dda0aae4a0dcdd7eaa7ea
Abimael-Lovera <abimaellovera@gmail.com>
2022-02-07 11:43:04 -0300

    Promocoes css bug

--- src/components/promocoes/Promocoes.jsx
+++ src/components/promocoes/Promocoes.jsx
@@ -24,181 +24,183 @@ export function Promocoes() {
 	return (
 		<>
 			<Navbar />
-			<div className="container" id="container">
-				<h1 className="title text-center">Pacotes imperdíveis para suas próximas férias</h1>
-				<p className="subtitle text-center">As melhores ofertas para você com preços especiais para destinos inesquecíveis!
-				</p>
-				<section className="cards__container">
-					<div className="cards__container-card">
-						<div className="cards__container-img">
-							<img src={rioDeJaneiro} alt="" />
+			<div className="container__promocoes">
+				<div className="container" id="container">
+					<h1 className="title text-center">Pacotes imperdíveis para suas próximas férias</h1>
+					<p className="subtitle text-center">As melhores ofertas para você com preços especiais para destinos inesquecíveis!
+					</p>
+					<section className="cards__container">
+						<div className="cards__container-card">
+							<div className="cards__container-img">
+								<img src={rioDeJaneiro} alt="" />
+							</div>
+							<h4>Rio de Janeiro</h4>
+							<p>
+								Saindo de Brasilia <br />
+								Hotel + Aereo <br />
+								3 noites
+							</p>
+							<a href="#" className="cards__container-btn">R$ 1000</a>
 						</div>
-						<h4>Rio de Janeiro</h4>
-						<p>
-							Saindo de Brasilia <br />
-							Hotel + Aereo <br />
-							3 noites
-						</p>
-						<a href="#" className="cards__container-btn">R$ 1000</a>
-					</div>
-					<div className="cards__container-card">
-						<div className="cards__container-img">
-							<img src={saopaulo} alt="" />
+						<div className="cards__container-card">
+							<div className="cards__container-img">
+								<img src={saopaulo} alt="" />
+							</div>
+							<h4>São Paulo</h4>
+							<p>
+								Saindo de Brasilia <br />
+								Hotel + Aereo <br />
+								3 noites
+							</p>
+							<a href="#" className="cards__container-btn">R$ 1000</a>
 						</div>
-						<h4>São Paulo</h4>
-						<p>
-							Saindo de Brasilia <br />
-							Hotel + Aereo <br />
-							3 noites
-						</p>
-						<a href="#" className="cards__container-btn">R$ 1000</a>
-					</div>
-					<div className="cards__container-card">
-						<div className="cards__container-img">
-							<img src={maceio} alt="" />
+						<div className="cards__container-card">
+							<div className="cards__container-img">
+								<img src={maceio} alt="" />
+							</div>
+							<h4>Maceió</h4>
+							<p>
+								Saindo de Brasilia <br />
+								Hotel + Aereo <br />
+								3 noites
+							</p>
+							<a href="#" className="cards__container-btn">R$ 1000</a>
 						</div>
-						<h4>Maceió</h4>
-						<p>
-							Saindo de Brasilia <br />
-							Hotel + Aereo <br />
-							3 noites
-						</p>
-						<a href="#" className="cards__container-btn">R$ 1000</a>
-					</div>
-					<div className="cards__container-card">
-						<div className="cards__container-img">
-							<img src={aracaju} alt="" />
+						<div className="cards__container-card">
+							<div className="cards__container-img">
+								<img src={aracaju} alt="" />
+							</div>
+							<h4>Aracaju</h4>
+							<p>
+								Saindo de Brasilia <br />
+								Hotel + Aereo <br />
+								3 noites
+							</p>
+							<a href="#" className="cards__container-btn">R$ 1000</a>
 						</div>
-						<h4>Aracaju</h4>
-						<p>
-							Saindo de Brasilia <br />
-							Hotel + Aereo <br />
-							3 noites
-						</p>
-						<a href="#" className="cards__container-btn">R$ 1000</a>
-					</div>
-					<div className="cards__container-card">
-						<div className="cards__container-img">
-							<img src={curitiba} alt="" />
+						<div className="cards__container-card">
+							<div className="cards__container-img">
+								<img src={curitiba} alt="" />
+							</div>
+							<h4>Curitiba</h4>
+							<p>
+								Saindo de Brasilia <br />
+								Hotel + Aereo <br />
+								3 noites
+							</p>
+							<a href="#" className="cards__container-btn">R$ 1000</a>
 						</div>
-						<h4>Curitiba</h4>
-						<p>
-							Saindo de Brasilia <br />
-							Hotel + Aereo <br />
-							3 noites
-						</p>
-						<a href="#" className="cards__container-btn">R$ 1000</a>
-					</div>
-					<div className="cards__container-card">
-						<div className="cards__container-img">
-							<img src={dubai} alt="" />
+						<div className="cards__container-card">
+							<div className="cards__container-img">
+								<img src={dubai} alt="" />
+							</div>
+							<h4>Dubai</h4>
+							<p>
+								Saindo de Brasilia <br />
+								Hotel + Aereo <br />
+								3 noites
+							</p>
+							<a href="#" className="cards__container-btn">R$ 1000</a>
 						</div>
-						<h4>Dubai</h4>
-						<p>
-							Saindo de Brasilia <br />
-							Hotel + Aereo <br />
-							3 noites
-						</p>
-						<a href="#" className="cards__container-btn">R$ 1000</a>
-					</div>
 
-					<div className="cards__container-card">
-						<div className="cards__container-img">
-							<img src={florianopolis} alt="" />
+						<div className="cards__container-card">
+							<div className="cards__container-img">
+								<img src={florianopolis} alt="" />
+							</div>
+							<h4>Florianopolis</h4>
+							<p>
+								Saindo de Brasilia <br />
+								Hotel + Aereo <br />
+								3 noites
+							</p>
+							<a href="#" className="cards__container-btn">R$ 1000</a>
 						</div>
-						<h4>Florianopolis</h4>
-						<p>
-							Saindo de Brasilia <br />
-							Hotel + Aereo <br />
-							3 noites
-						</p>
-						<a href="#" className="cards__container-btn">R$ 1000</a>
-					</div>
-					<div className="cards__container-card">
-						<div className="cards__container-img">
-							<img src={fortaleza} alt="" />
+						<div className="cards__container-card">
+							<div className="cards__container-img">
+								<img src={fortaleza} alt="" />
+							</div>
+							<h4>Fortaleza</h4>
+							<p>
+								Saindo de Brasilia <br />
+								Hotel + Aereo <br />
+								3 noites
+							</p>
+							<a href="#" className="cards__container-btn">R$ 1000</a>
 						</div>
-						<h4>Fortaleza</h4>
-						<p>
-							Saindo de Brasilia <br />
-							Hotel + Aereo <br />
-							3 noites
-						</p>
-						<a href="#" className="cards__container-btn">R$ 1000</a>
-					</div>
-					<div className="cards__container-card">
-						<div className="cards__container-img">
-							<img src={fozDoIguacu} alt="" />
+						<div className="cards__container-card">
+							<div className="cards__container-img">
+								<img src={fozDoIguacu} alt="" />
+							</div>
+							<h4>Foz do Iguaçu</h4>
+							<p>
+								Saindo de Brasilia <br />
+								Hotel + Aereo <br />
+								3 noites
+							</p>
+							<a href="#" className="cards__container-btn">R$ 1000</a>
 						</div>
-						<h4>Foz do Iguaçu</h4>
-						<p>
-							Saindo de Brasilia <br />
-							Hotel + Aereo <br />
-							3 noites
-						</p>
-						<a href="#" className="cards__container-btn">R$ 1000</a>
-					</div>
-					<div className="cards__container-card">
-						<div className="cards__container-img">
-							<img src={joaoPessoa} alt="" />
+						<div className="cards__container-card">
+							<div className="cards__container-img">
+								<img src={joaoPessoa} alt="" />
+							</div>
+							<h4>João Pessoa</h4>
+							<p>
+								Saindo de Brasilia <br />
+								Hotel + Aereo <br />
+								3 noites
+							</p>
+							<a href="#" className="cards__container-btn">R$ 1000</a>
 						</div>
-						<h4>João Pessoa</h4>
-						<p>
-							Saindo de Brasilia <br />
-							Hotel + Aereo <br />
-							3 noites
-						</p>
-						<a href="#" className="cards__container-btn">R$ 1000</a>
-					</div>
-					<div className="cards__container-card">
-						<div className="cards__container-img">
-							<img src={lisboa} alt="" />
+						<div className="cards__container-card">
+							<div className="cards__container-img">
+								<img src={lisboa} alt="" />
+							</div>
+							<h4>Lisboa</h4>
+							<p>
+								Saindo de Brasilia <br />
+								Hotel + Aereo <br />
+								3 noites
+							</p>
+							<a href="#" className="cards__container-btn">R$ 1000</a>
 						</div>
-						<h4>Lisboa</h4>
-						<p>
-							Saindo de Brasilia <br />
-							Hotel + Aereo <br />
-							3 noites
-						</p>
-						<a href="#" className="cards__container-btn">R$ 1000</a>
-					</div>
-					<div className="cards__container-card">
-						<div className="cards__container-img">
-							<img src={natal} alt="" />
+						<div className="cards__container-card">
+							<div className="cards__container-img">
+								<img src={natal} alt="" />
+							</div>
+							<h4>Natal</h4>
+							<p>
+								Saindo de Brasilia <br />
+								Hotel + Aereo <br />
+								3 noites
+							</p>
+							<a href="#" className="cards__container-btn">R$ 1000</a>
 						</div>
-						<h4>Natal</h4>
-						<p>
-							Saindo de Brasilia <br />
-							Hotel + Aereo <br />
-							3 noites
-						</p>
-						<a href="#" className="cards__container-btn">R$ 1000</a>
-					</div>
-					<div className="cards__container-card">
-						<div className="cards__container-img">
-							<img src={orlando} alt="" />
+						<div className="cards__container-card">
+							<div className="cards__container-img">
+								<img src={orlando} alt="" />
+							</div>
+							<h4>Orlando</h4>
+							<p>
+								Saindo de Brasilia <br />
+								Hotel + Aereo <br />
+								3 noites
+							</p>
+							<a href="#" className="cards__container-btn">R$ 1000</a>
 						</div>
-						<h4>Orlando</h4>
-						<p>
-							Saindo de Brasilia <br />
-							Hotel + Aereo <br />
-							3 noites
-						</p>
-						<a href="#" className="cards__container-btn">R$ 1000</a>
-					</div>
-					<div className="cards__container-card">
-						<div className="cards__container-img">
-							<img src={salvador} alt="" />
+						<div className="cards__container-card">
+							<div className="cards__container-img">
+								<img src={salvador} alt="" />
+							</div>
+							<h4>Salvador</h4>
+							<p>
+								Saindo de Brasilia <br />
+								Hotel + Aereo <br />
+								3 noites
+							</p>
+							<a href="#" className="cards__container-btn">R$ 1000</a>
 						</div>
-						<h4>Salvador</h4>
-						<p>
-							Saindo de Brasilia <br />
-							Hotel + Aereo <br />
-							3 noites
-						</p>
-						<a href="#" className="cards__container-btn">R$ 1000</a>
-					</div>
-				</section>
+					</section>
+				</div>
 			</div>
 			<Footer />
 		</>
