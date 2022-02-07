d1ea17475bd1745d68a86672537d16b32811ecc0
Abimael-Lovera <abimaellovera@gmail.com>
2022-02-07 11:13:14 -0300

    Navbar custom colors

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export function Navbar() {
 	return (
-		<nav className="navbar fixed-top navbar-expand-lg navbar-light p-md-3">
+		<nav className="navbar bg-light	fixed-top navbar-expand-lg navbar-light p-md-3" id="navbar">
 			<div className="container">
 				<Link className="navbar-brand nav_logo" to="/home">
 					<img src={logo} alt="logo-lovera" />
