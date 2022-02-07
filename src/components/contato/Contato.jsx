3d3221e4778942774a3deab23dd280ed5f8ed1e2
Abimael-Lovera <abimaellovera@gmail.com>
2022-02-06 22:45:47 -0300

    Contato html customization

--- src/components/contato/Contato.jsx
+++ src/components/contato/Contato.jsx
@@ -1,5 +1,23 @@
+import { Footer } from '../footer/Footer';
+import { Navbar } from '../navbar/Navbar';
 import './Contato.css';
 
 export function Contato() {
-	return <h1>Contato</h1>;
+	return (
+		<>
+			<Navbar />
+			<section className="contact">
+				<form className="form" action="https://formspree.io/f/xrgjgyed" method="POST">
+					<div className="form_container">
+						<h2 className="form_title">Contatenos</h2>
+						<input type="text" className="form_input" placeholder="Nome" name="name" />
+						<input type="email" className="form_input" placeholder="Email" name="email" />
+						<textarea className="form_input form_input-message" placeholder="Escreva seu messagem" name="message"></textarea>
+						<input type="submit" className="form_cta" value="Enviar" />
+					</div>
+				</form>
+			</section>
+			<Footer />
+		</>
+	);
 }
