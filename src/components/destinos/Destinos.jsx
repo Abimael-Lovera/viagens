9ff344beb48d9679f2f5b0c465d8012ec8004063
Abimael-Lovera <abimaellovera@gmail.com>
2022-02-07 11:27:01 -0300

    Destinos css Bug container

--- src/components/destinos/Destinos.jsx
+++ src/components/destinos/Destinos.jsx
@@ -75,120 +75,122 @@ export function Destinos() {
 				</div>
 			</section>
 			{/* CARDS  */}
-			<div className="container" id="container">
-				<h1 className="title text-center">Pacotes imperdíveis para suas próximas férias</h1>
-				<p className="subtitle text-center">As melhores ofertas para você com preços especiais para destinos inesquecíveis!
-				</p>
-				<section className="cards_container">
-					<div className="cards_container-card">
-						<img src={saopaulo} alt="" />
-						<div className="cards_container-body">
-							<h4>Sao paulo</h4>
-							<p>
-								Brasil <br />
-								<i className="fa-solid fa-arrow-down"></i>
-							</p>
-							<a href="#" className="cards_container-btn"> Promoções<i
-								className="fa-solid fa-arrow-down-to-square"></i></a>
+			<div className="container__destinos">
+				<div className="container" id="container">
+					<h1 className="title text-center">Pacotes imperdíveis para suas próximas férias</h1>
+					<p className="subtitle text-center">As melhores ofertas para você com preços especiais para destinos inesquecíveis!
+					</p>
+					<section className="cards_container">
+						<div className="cards_container-card">
+							<img src={saopaulo} alt="" />
+							<div className="cards_container-body">
+								<h4>Sao paulo</h4>
+								<p>
+									Brasil <br />
+									<i className="fa-solid fa-arrow-down"></i>
+								</p>
+								<a href="#" className="cards_container-btn"> Promoções<i
+									className="fa-solid fa-arrow-down-to-square"></i></a>
+							</div>
 						</div>
-					</div>
-					<div className="cards_container-card">
-						<img src={rioDeJaneiro} alt="" />
-						<div className="cards_container-body">
-							<h4>Rio de Janerio</h4>
-							<p>
-								Brasil <br />
-								<i className="fa-solid fa-arrow-down"></i>
-							</p>
-							<a href="#" className="cards_container-btn"> Promoções<i
-								className="fa-solid fa-arrow-down-to-square"></i></a>
+						<div className="cards_container-card">
+							<img src={rioDeJaneiro} alt="" />
+							<div className="cards_container-body">
+								<h4>Rio de Janerio</h4>
+								<p>
+									Brasil <br />
+									<i className="fa-solid fa-arrow-down"></i>
+								</p>
+								<a href="#" className="cards_container-btn"> Promoções<i
+									className="fa-solid fa-arrow-down-to-square"></i></a>
+							</div>
 						</div>
-					</div>
-					<div className="cards_container-card">
-						<img src={lisboa} alt="" />
-						<div className="cards_container-body">
-							<h4>Lisboa</h4>
-							<p>
-								Portugal <br />
-								<i className="fa-solid fa-arrow-down"></i>
-							</p>
-							<a href="#" className="cards_container-btn"> Promoções<i
-								className="fa-solid fa-arrow-down-to-square"></i></a>
+						<div className="cards_container-card">
+							<img src={lisboa} alt="" />
+							<div className="cards_container-body">
+								<h4>Lisboa</h4>
+								<p>
+									Portugal <br />
+									<i className="fa-solid fa-arrow-down"></i>
+								</p>
+								<a href="#" className="cards_container-btn"> Promoções<i
+									className="fa-solid fa-arrow-down-to-square"></i></a>
+							</div>
 						</div>
-					</div>
-					<div className="cards_container-card">
-						<img src={natal} alt="" />
-						<div className="cards_container-body">
-							<h4>Natal</h4>
-							<p>
-								Brasil <br />
-								<i className="fa-solid fa-arrow-down"></i>
-							</p>
-							<a href="#" className="cards_container-btn"> Promoções<i
-								className="fa-solid fa-arrow-down-to-square"></i></a>
+						<div className="cards_container-card">
+							<img src={natal} alt="" />
+							<div className="cards_container-body">
+								<h4>Natal</h4>
+								<p>
+									Brasil <br />
+									<i className="fa-solid fa-arrow-down"></i>
+								</p>
+								<a href="#" className="cards_container-btn"> Promoções<i
+									className="fa-solid fa-arrow-down-to-square"></i></a>
+							</div>
 						</div>
-					</div>
-					<div className="cards_container-card">
-						<img src={orlando} alt="" />
-						<div className="cards_container-body">
-							<h4>Orlando</h4>
-							<p>
-								EEUU <br />
-								<i className="fa-solid fa-arrow-down"></i>
-							</p>
-							<a href="#" className="cards_container-btn"> Promoções<i
-								className="fa-solid fa-arrow-down-to-square"></i></a>
+						<div className="cards_container-card">
+							<img src={orlando} alt="" />
+							<div className="cards_container-body">
+								<h4>Orlando</h4>
+								<p>
+									EEUU <br />
+									<i className="fa-solid fa-arrow-down"></i>
+								</p>
+								<a href="#" className="cards_container-btn"> Promoções<i
+									className="fa-solid fa-arrow-down-to-square"></i></a>
+							</div>
 						</div>
-					</div>
-					<div className="cards_container-card">
-						<img src={maceio1} alt="" />
-						<div className="cards_container-body">
-							<h4>Maceió</h4>
-							<p>
-								Brasil <br />
-								<i className="fa-solid fa-arrow-down"></i>
-							</p>
-							<a href="#" className="cards_container-btn"> Promoções<i
-								className="fa-solid fa-arrow-down-to-square"></i></a>
+						<div className="cards_container-card">
+							<img src={maceio1} alt="" />
+							<div className="cards_container-body">
+								<h4>Maceió</h4>
+								<p>
+									Brasil <br />
+									<i className="fa-solid fa-arrow-down"></i>
+								</p>
+								<a href="#" className="cards_container-btn"> Promoções<i
+									className="fa-solid fa-arrow-down-to-square"></i></a>
+							</div>
 						</div>
-					</div>
-					<div className="cards_container-card">
-						<img src={fozDoIguacu} alt="" />
-						<div className="cards_container-body">
-							<h4>Foz do Iguaçu</h4>
-							<p>
-								Brasil <br />
-								<i className="fa-solid fa-arrow-down"></i>
-							</p>
-							<a href="#" className="cards_container-btn"> Promoções<i
-								className="fa-solid fa-arrow-down-to-square"></i></a>
+						<div className="cards_container-card">
+							<img src={fozDoIguacu} alt="" />
+							<div className="cards_container-body">
+								<h4>Foz do Iguaçu</h4>
+								<p>
+									Brasil <br />
+									<i className="fa-solid fa-arrow-down"></i>
+								</p>
+								<a href="#" className="cards_container-btn"> Promoções<i
+									className="fa-solid fa-arrow-down-to-square"></i></a>
+							</div>
 						</div>
-					</div>
-					<div className="cards_container-card">
-						<img src={joaoPessoa} alt="" />
-						<div className="cards_container-body">
-							<h4>João Pessoa</h4>
-							<p>
-								Brasil <br />
-								<i className="fa-solid fa-arrow-down"></i>
-							</p>
-							<a href="#" className="cards_container-btn"> Promoções<i
-								className="fa-solid fa-arrow-down-to-square"></i></a>
+						<div className="cards_container-card">
+							<img src={joaoPessoa} alt="" />
+							<div className="cards_container-body">
+								<h4>João Pessoa</h4>
+								<p>
+									Brasil <br />
+									<i className="fa-solid fa-arrow-down"></i>
+								</p>
+								<a href="#" className="cards_container-btn"> Promoções<i
+									className="fa-solid fa-arrow-down-to-square"></i></a>
+							</div>
 						</div>
-					</div>
-					<div className="cards_container-card">
-						<img src={salvador1} alt="" />
-						<div className="cards_container-body">
-							<h4>Salvador</h4>
-							<p>
-								Brasil <br />
-								<i className="fa-solid fa-arrow-down"></i>
-							</p>
-							<a href="#" className="cards_container-btn"> Promoções<i
-								className="fa-solid fa-arrow-down-to-square"></i></a>
+						<div className="cards_container-card">
+							<img src={salvador1} alt="" />
+							<div className="cards_container-body">
+								<h4>Salvador</h4>
+								<p>
+									Brasil <br />
+									<i className="fa-solid fa-arrow-down"></i>
+								</p>
+								<a href="#" className="cards_container-btn"> Promoções<i
+									className="fa-solid fa-arrow-down-to-square"></i></a>
+							</div>
 						</div>
-					</div>
-				</section>
+					</section>
+				</div>
 			</div>
 			<Footer />
 		</>
