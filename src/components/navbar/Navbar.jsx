8e8a7f0d175deb46dec159894dd82ca9e328d0c4
Abimael-Lovera <abimaellovera@gmail.com>
2022-02-07 10:46:20 -0300

    Navbar Bug link

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import './Navbar.css';
 import logo from '../../assets/img/lovera+.png';
+import { Link } from 'react-router-dom';
 
 export function Navbar() {
 	return (
 		<nav className="navbar fixed-top navbar-expand-lg navbar-light p-md-3">
 			<div className="container">
-				<a className="navbar-brand nav_logo" asp-action="Index">
+				<Link className="navbar-brand nav_logo" to="/home">
 					<img src={logo} alt="logo-lovera" />
-				</a>
+				</Link>
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
 					aria-expanded="false" aria-label="Toggle navigation">
 
@@ -18,26 +19,33 @@ export function Navbar() {
 					<div className="mx-auto"></div>
 					<ul className="navbar-nav">
 						<li className="nav-item">
-							<a className="nav-link nav_links" asp-action="Destinos"> <i
-								className="fa-solid fa-plane-departure"></i>
-								Destinos </a>
+							<Link className="nav-link nav_links" to="/destinos">
+								<i className="fa-solid fa-plane-departure"></i>
+								Destinos
+							</Link>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link nav_links" asp-action="Promo"> <i
-								className="fa-solid fa-briefcase"></i>
-								Promoções </a>
+							<Link className="nav-link nav_links" to="/promocoes">
+								<i className="fa-solid fa-briefcase"></i>
+								Promoções
+							</Link>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link nav_links" asp-action="Contact"> <i
-								className="fa-solid fa-address-book"></i>
-								Contato </a>
+							<Link className="nav-link nav_links" to="/contato">
+								<i className="fa-solid fa-address-book"></i>
+								Contato
+							</Link>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link nav_links" asp-action="Login"> Login <i
-								className="fa-solid fa-right-to-bracket"></i> </a>
+							<Link className="nav-link nav_links" to="/login">
+								Login
+								<i className="fa-solid fa-right-to-bracket"></i>
+							</Link>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link nav_links nav_btn" asp-action="Login">Criar Conta</a>
+							<Link className="nav-link nav_links nav_btn" to="/register">
+								Criar Conta
+							</Link>
 						</li>
 
 					</ul>
